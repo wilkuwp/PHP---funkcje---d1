f280fb1a4c33aae5f17ea5ebea626a516f50b405
Patryk Wolf <patryk.wolf@gmail.com>
2022-10-01 08:08:56 +0000

    Initial commit from PHPSandbox

---
+++ index.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Witaj na kursie SEO</title>
+    <link rel="icon" href="https://phpsandbox.io/assets/img/brand/phpsandbox.png">
+    <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,400;0,500;0,531;0,600;0,700;0,800;0,900;1,400;1,500;1,531;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="bg-white font-sans text-gray-700" style="font-family: 'Jost', sans-serif">
+    <div class="font-medium">
+        <div>
+            <h1 style="font-size: 2rem;" class="text-gray-700"> Poniżej znajdziesz linki do wszystkich zadań:</h1>
+            <h2 style="font-size: 1.5rem;">Funkcje:</h2>
+            <ul class="list-disc pl-8">
+                <li><a href="./01_Funkcje/01_Zadanie_1/zadanie1.php">Zadanie 1</a></li>
+                <li><a href="./01_Funkcje/02_Zadanie_2/zadanie2.php">Zadanie 2</a></li>
+                <li><a href="./01_Funkcje/03_Zadanie_3/zadanie3.php">Zadanie 3</a></li>
+                <li><a href="./01_Funkcje/04_Zadanie_4/zadanie4.php">Zadanie 4</a></li>
+                <li><a href="./01_Funkcje/04_Zadanie_4/zadanie4.php">Zadanie 4</a></li>
+            </ul>
+
+            <h2 style="font-size: 1.5rem;">Zmienne tematy zaawansowane:</h2>
+            <ul class="list-disc pl-8">
+                <li><a href="./02_Zmienne_tematy_zaawansowane/01_Zadanie_1/zadanie1.php">Zadanie 1</a></li>
+                <li><a href="./02_Zmienne_tematy_zaawansowane/02_Zadanie_2/zadanie2.php">Zadanie 2</a></li>
+                <li><a href="./02_Zmienne_tematy_zaawansowane/03_Zadanie_3/zadanie3.php">Zadanie 3</a></li>
+                <li><a href="./02_Zmienne_tematy_zaawansowane/04_Zadanie_4/zadanie4.php">Zadanie 4</a></li>
+                <li><a href="./02_Zmienne_tematy_zaawansowane/05_Zadanie_5/zadanie5.php">Zadanie 5</a></li>
+            </ul>
+
+            <h2 style="font-size: 1.5rem;">Napisy i przydatne funkcje:</h2>
+            <ul class="list-disc pl-8">
+                <li><a href="./03_Napisy_i_przydatne_funkcje/01_Zadanie_1/zadanie1.php">Zadanie 1</a></li>
+                <li><a href="./03_Napisy_i_przydatne_funkcje/02_Zadanie_2/zadanie2.php">Zadanie 2</a></li>
+                <li><a href="./03_Napisy_i_przydatne_funkcje/03_Zadanie_3/zadanie3.php">Zadanie 3</a></li>
+                <li><a href="./03_Napisy_i_przydatne_funkcje/04_Zadanie_4/zadanie4.php">Zadanie 4</a></li>
+                <li><a href="./03_Napisy_i_przydatne_funkcje/05_Zadanie_5/zadanie5.php">Zadanie 5</a></li>
+            </ul>
+
+            <h2 style="font-size: 1.5rem;">Tablice:</h2>
+            <ul class="list-disc pl-8">
+                <li><a href="./04_Tablice/01_Zadanie_1/zadanie1.php">Zadanie 1</a></li>
+                <li><a href="./04_Tablice/02_Zadanie_2/zadanie2.php">Zadanie 2</a></li>
+                <li><a href="./04_Tablice/03_Zadanie_3/zadanie3.php">Zadanie 3</a></li>
+                <li><a href="./04_Tablice/04_Zadanie_4/zadanie4.php">Zadanie 4</a></li>
+                <li><a href="./04_Tablice/05_Zadanie_5/zadanie5.php">Zadanie 5</a></li>
+            </ul>
+
+
+            <h2 style="font-size: 1.5rem;">Czas i daty:</h2>
+            <ul class="list-disc pl-8">
+                <li><a href="./05_Czas_i_daty_w_PHP/01_Zadanie_1/zadanie1.php">Zadanie 1</a></li>
+                <li><a href="./05_Czas_i_daty_w_PHP/02_Zadanie_2/zadanie2.php">Zadanie 2</a></li>
+                <li><a href="./05_Czas_i_daty_w_PHP/03_Zadanie_3/zadanie3.php">Zadanie 3</a></li>
+                <li><a href="./05_Czas_i_daty_w_PHP/04_Zadanie_4/zadanie4.php">Zadanie 4</a></li>
+                <li><a href="./05_Czas_i_daty_w_PHP/05_Zadanie_5/zadanie5.php">Zadanie 5</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+</body>
+</html>
